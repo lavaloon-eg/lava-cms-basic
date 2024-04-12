1aa88c83af25c77966f71581b23a44daf4c23805
khaled Yusuf <khaled.yusuf@lavaloon.com>
2024-04-12 04:04:58 +0200

    create the patch of migrating the translation records

--- lava_cms_basic/lava_cms_basic/doctype/website_page/website_page.py
+++ lava_cms_basic/lava_cms_basic/doctype/website_page/website_page.py
@@ -3,6 +3,7 @@
 import frappe
 # import frappe
 from frappe.model.document import Document
+from lava_cms_basic.patches._2024_4_12_migrate_translation_records import migrate_translation_data
 
 
 class WebsitePage(Document):
